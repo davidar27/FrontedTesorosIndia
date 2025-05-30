482ccf17c8060afd0a362eabd4f3261b5e876b8c
davidar27 <jhonatanarcos10@gmail.com>
2025-05-30 16:41:05 -0500

    refactor: change CreateFarmsConfig export to default; update import statement in FarmSection for consistency

--- src/features/admin/farms/FarmSection.tsx
+++ src/features/admin/farms/FarmSection.tsx
@@ -9,7 +9,7 @@ import { useProtectedMutation } from '@/hooks/useProtectedMutation';
 import { Farm } from '@/features/admin/farms/FarmTypes';
 import { farmsApi } from '@/services/admin/farms';
 import FarmCard from '@/features/admin/farms/FamCard'
-import { CreateFarmsConfig } from './CreateFarmsConfig';
+import CreateFarmsConfig from '@/features/admin/farms/CreateFarmsConfig';
 
 export default function FarmsManagement() {
     const queryClient = useQueryClient();
