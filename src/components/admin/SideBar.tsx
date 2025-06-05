3895b75dff3e9b9525574b9af1950445b7646580
davidar27 <jhonatanarcos10@gmail.com>
2025-06-05 17:41:56 -0500

    refactor: update DashboardLayout and SideBar components to replace Filter icon with Menu; enhance SideBar layout and integrate UserMenu for improved mobile navigation; remove unused SideBarItems and sidebarItems constants for cleaner code

--- src/components/admin/SideBar.tsx
+++ src/components/admin/SideBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {
-    LucideIcon,
-} from 'lucide-react';
+
 import logo from '@/assets/icons/logotesorosindiaPequeño.webp';
 import Picture from '@/components/ui/display/Picture';
 import { useNavigate } from 'react-router-dom';
+import { LucideIcon } from 'lucide-react';
+import UserMenu from '../layouts/menu/UserMenu';
 
 export interface SidebarItem {
     id: string;
@@ -19,33 +19,40 @@ const Sidebar: React.FC<{
     showMobileMenu: boolean;
     onItemClick: (itemId: string) => void;
     onMobileMenuClose: () => void;
-    
+
 }> = React.memo(({ items, showMobileMenu, onItemClick, onMobileMenuClose }) => {
     const navigate = useNavigate();
 
-    
+
     return (
         <>
-            <aside className={`bg-white border-r border-gray-200 transition-all duration-300 shadow-lg ${showMobileMenu ? 'translate-x-0' : '-translate-x-full'
-                } lg:translate-x-0 lg:relative z-30 w-64 h-screen`}>
+            <aside className={`bg-white border-r border-gray-200 transition-all duration-300 shadow-lg
+                ${showMobileMenu ? 'fixed left-0 top-0 translate-x-0 overflow-y-auto' : 'fixed left-0 top-0 -translate-x-full'}
+                lg:translate-x-0 lg:relative lg:left-auto lg:top-auto
+                z-30 w-64 h-screen`}>
                 <div className="p-6 border-b border-gray-200  ">
-                    <div 
+                    <div
                         onClick={() => navigate('/')}
-                        className="flex items-center gap-3 animate-fade-in-right cursor-pointer hover:bg-gray-50 transition-all duration-200 rounded-xl p-2">
-                        
+                        className="flex items-center gap-3 animate-fade-in-right cursor-pointer hover:bg-gray-50 transition-all duration-200 ">
+
                         <Picture
                             src={logo}
                             alt="Logo"
-                                className="w-14 h-14 object-center object-contain"
-                            />
+                            className="w-10 h-10 object-center object-contain"
+                        />
                         <div>
-                            <h1 className="font-bold text-gray-800 text-xl">Tesoros de la India</h1>
+                            <h1 className="font-bold text-gray-800 text-md lg:text-xl">Tesoros de la India</h1>
                             <p className="text-sm text-gray-500 ">Gestión del Sistema</p>
                         </div>
                     </div>
                 </div>
 
                 <nav className="p-4">
+                    <span className='sm:hidden'>
+                        <UserMenu
+                            textColor='!black'
+                        />
+                    </span>
                     <ul className="space-y-2">
                         {items.map((item, index) => (
                             <li
@@ -55,9 +62,9 @@ const Sidebar: React.FC<{
                             >
                                 <button
                                     onClick={() => onItemClick(item.id)}
-                                    className={`w-full flex items-center gap-3 px-4 py-3 rounded-xl transition-all duration-200 ${item.active
-                                            ? 'bg-primary text-white shadow-lg'
-                                            : 'text-gray-600 hover:bg-gray-50 hover:text-primary'
+                                    className={`w-full flex items-center gap-3 px-4 py-3 rounded-xl transition-all duration-200 cursor-pointer ${item.active
+                                        ? 'bg-primary text-white shadow-lg'
+                                        : 'text-gray-600 hover:bg-gray-100 hover:text-primary'
                                         }`}
                                 >
                                     <item.icon className="w-5 h-5" />
